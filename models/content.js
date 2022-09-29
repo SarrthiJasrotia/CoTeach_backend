e9e07b97e1c3c37a176afb4d65acc47382482565
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-29 14:16:45 -0400

    schema naming fixed

--- models/content.js
+++ models/content.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 
-const contentSchema = new mongoose.Schema({
+const ContentSchema = new mongoose.Schema({
     title: String,
     teacher: String,
     videoURL: String,
@@ -11,5 +11,6 @@ const contentSchema = new mongoose.Schema({
     resources: String,
     category: String,
 })
+const Content = mongoose.model("Content" , ContentSchema);
 
-module.exports = contentSchema;
+module.exports = Content;
